cde5e42b6a7f635ee73af84995e614660b39d279
aniketparate <aniketvparate@gmail.com>
2022-11-10 23:17:58 +0530

    added proto1 Co-authored-by: Dishant Wakalkar <dishantwakalkar@gmail.com>

--- excelData/main.py
+++ excelData/main.py
@@ -1,5 +1,64 @@
 import pandas as pd
+import re
 
-data = pd.read_excel('magnet_brain_hindi.xlsx')
+def use_regex(input_text):
+    pattern = re.compile("हिंदी", re.IGNORECASE)
+    a = pattern.search(input_text)
 
-print(data)
+    if a:
+        print(True)
+    else:
+        print(False)
+
+
+def findClass(input_text):
+    pattern1 = re.compile("class [0-9]+", re.IGNORECASE)
+    pattern2 = re.compile("कक्षा [0-9]+", re.IGNORECASE)
+
+    srh1 = pattern1.search(input_text)
+    srh2 = pattern2.search(input_text)
+
+    if srh1:
+        print(srh1[0].split()[1])
+    elif srh2:
+        print(srh2[0].split()[1])
+    else:
+        print(None)
+        # print(input_text)
+
+
+def findMedium(input_text):
+    string = "[A-Z][a-z]+(?= medium)", "(?<= medium)[A-Z][a-z]+"
+    medium = "hindi", "english", "हिंदी"
+    num = string.__len__()
+    i = 0
+    while i != num:
+        pattern = re.compile(string[i], re.IGNORECASE)
+        srh = pattern.search(input_text)
+        if srh:
+            print(True)
+            i+=1
+        else:
+            print(False)
+            i+=1
+
+    # str = 'Class 12 Hindi (Core) Hindi Medium Hello सभी State Boards के लिए। सभी पाठ उपलब्ध'
+    
+
+    # a = pattern1.search(input_text)
+
+    
+
+def findSubject():
+    pass
+
+def findAuthor():
+    pass
+
+file = "magnet_brain_hindi.xlsx"
+data = pd.read_excel(file, usecols=['playlist_name'])
+
+
+for i in data.itertuples():
+    print(i[0])
+    findMedium(i[1])
---
+++ excelData/proto1.py
@@ -0,0 +1,27 @@
+# from translate import Translator
+# translator= Translator(to_lang="english")
+# translation = translator.translate("Class 12 Hindi (Core) हिंदी Medium | सभी State Boards के लिए। सभी पाठ उपलब्ध")
+# print (translation)
+
+# हिंदी सभी के लिए सभी पाठ उपलब्ध
+# Class 12 Hindi (Core) हिंदी Medium | सभी State Boards के लिए। सभी पाठ उपलब्ध
+
+
+# import re
+# pattern = re.compile('हिंदी')
+# str = 'Class 12 Hindi (Core) हिंदी Medium | सभी State Boards के लिए। सभी पाठ उपलब्ध'
+# a = pattern.search(str)
+# print(a)
+# print(a[0])
+
+
+# import re
+# pattern = re.compile("[A-Z][a-z]+(?= medium)","(?<= medium)[A-Z][a-z]+", re.IGNORECASE)
+# str = 'Class 12 Hindi (Core) Hindi Medium Hello सभी State Boards के लिए। सभी पाठ उपलब्ध'
+# print(pattern.search(str))
+
+import re
+pattern = re.compile("[\u0900-\u097F]+(?= medium)", re.IGNORECASE)
+# pattern = re.compile("[\u0900-\u097F]+(?= medium)", re.IGNORECASE)
+str = 'Class 12 Hindi (Core) Hindi सभी हिंदी Medium State Boards के लिए। सभी पाठ उपलब्ध'
+print(pattern.search(str))
