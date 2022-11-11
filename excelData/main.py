9ae830773dee1ad01820c562605344f5bcccaaea
DishantWakalkar <dishantwakalkar@gmail.com>
2022-11-11 23:10:41 +0530

    Updated

--- excelData/main.py
+++ excelData/main.py
@@ -1,35 +1,36 @@
 import pandas as pd
 import re
 
-def use_regex(input_text):
-    pattern = re.compile("हिंदी", re.IGNORECASE)
-    a = pattern.search(input_text)
+# def use_regex(input_text):
+#     pattern = re.compile("हिंदी", re.IGNORECASE)
+#     a = pattern.search(input_text)
 
-    if a:
-        print(True)
-    else:
-        print(False)
+#     if a:
+#         print(True)
+#     else:
+#         print(False)
 
 
 def findClass(input_text):
-    pattern1 = re.compile("class [0-9]+", re.IGNORECASE)
-    pattern2 = re.compile("कक्षा [0-9]+", re.IGNORECASE)
-
-    srh1 = pattern1.search(input_text)
-    srh2 = pattern2.search(input_text)
-
-    if srh1:
-        print(srh1[0].split()[1])
-    elif srh2:
-        print(srh2[0].split()[1])
-    else:
-        print(None)
-        # print(input_text)
+    string = "(?<=class )\d+", "\d+|\s+(?= कक्षा)", "(?<=कक्षा )\d+", "\d+|\s+(?= class)"
+    i = 0
+    num = string.__len__()
+    while i != num:
+        pattern = re.compile(string[i], re.IGNORECASE)
+        srh = pattern.search(input_text)
+    
+        if srh == None:
+            pass
+            i+=1
+        else:
+            print(i)
+            print(srh[0])
+            i+=1
 
 
 def findMedium(input_text):
-    string = "[\u0900-\u097F]+(?= medium)", "[A-Z][a-z]+(?= medium)", "(?<= medium)[A-Z][a-z]+"
-    medium = "hindi", "english", "हिंदी"
+    string = "[\u0900-\u097F]+(?= medium)", "\w+(?= medium)", "(?<= medium)\w+", "(?<= medium )[\u0900-\u097F]+"
+    medium = "hindi", "english", "हिंदी", "अंग्रेज़ी", "एन्ग्लिश्"
     num = string.__len__()
     i = 0
     while i != num:
@@ -46,33 +47,31 @@ def findMedium(input_text):
             else:
                 print(False, med, srh[0])
                 i+=1
-
-
-        # if srh:
-        #     print(True)
-        #     i+=1
-        # else:
-        #     print(False)
-        #     i+=1
-
-    # str = 'Class 12 Hindi (Core) Hindi Medium Hello सभी State Boards के लिए। सभी पाठ उपलब्ध'
     
+def findSubject(input_text):
+    string = "(?<=class )[0-9]+ [A-Z][a-z]+", "(?<=class )[0-9]+[A-Z][a-z]+ [A-Z][a-z]+", "(?<=class )[0-9]+[A-Z][a-z]+ [\u0900-\u097F]+", "(?<=class )[0-9]+ [\u0900-\u097F]+"
+    i = 0
+    num = string.__len__()
+    while i != num:
+        pattern = re.compile(string[i], re.IGNORECASE)
+        srh = pattern.search(input_text)
 
-    # a = pattern1.search(input_text)
-
-    
-
-def findSubject():
-    pass
+        if srh == None:
+            i+=1
+            pass
+        else:
+            i+=1
+            print(srh[0].split()[1])
 
 def findAuthor():
+
     pass
 
 file = "magnet_brain_hindi.xlsx"
-data = pd.read_excel(file, usecols=['playlist_name'])
+dataPlaylistName = pd.read_excel(file, usecols=['playlist_name', 'asset_title'])
 
-
-for i in data.itertuples():
+for i in dataPlaylistName.itertuples():
     print(i[0])
-    findMedium(i[1])
-    # findClass(i[1])
+    # findMedium(i[1])
+    findClass(i[1])
+    # findSubject(i[2])
--- excelData/proto1.py
+++ excelData/proto1.py
@@ -19,11 +19,30 @@
 # pattern = re.compile("[A-Z][a-z]+(?= medium)","(?<= medium)[A-Z][a-z]+", re.IGNORECASE)
 # str = 'Class 12 Hindi (Core) Hindi Medium Hello सभी State Boards के लिए। सभी पाठ उपलब्ध'
 # print(pattern.search(str))
+# import re
+# pattern = re.compile("[A-Z][a-z]+(?= medium)", re.IGNORECASE)
+# str = 'Class 12 Hindi (Core) Hindi Medium Hello सभी State Boards के लिए। सभी पाठ उपलब्ध'
+# a=pattern.search(str)
+# print(a[0])
+
+
+# import re
+# pattern = re.compile("[\u0900-\u097F]+(?= medium)", re.IGNORECASE)
+# pattern = re.compile("(?<= medium)[\u0900-\u097F]+", re.IGNORECASE)
+# str = 'Class 12 Hindi (Core) Hindi हिंदी Medium लिए सभी पाठ उपलब्ध'
+# a = pattern.search(str)
+# print(a[0])
+# Class 12 Hindi (Core) Hindi हिंदी Medium लिए । सभी पाठ उपलब्ध
 
 import re
-pattern = re.compile("[\u0900-\u097F]+(?= medium)", re.IGNORECASE)
-# pattern = re.compile("(?<= medium)[\u0900-\u097F]+", re.IGNORECASE)
-str = 'Class 12 Hindi (Core) Hindi हिंदी Medium लिए सभी पाठ उपलब्ध'
-a = pattern.search(str)
-print(a[0])
-# Class 12 Hindi (Core) Hindi हिंदी Medium लिए । सभी पाठ उपलब्ध
+# pattern = re.compile("(?<= medium )[\u0900-\u097F]+", re.IGNORECASE)
+# str = 'Class 12 Hindi (Core) हिंदी Medium  | सभी State Boards के लिए। सभी पाठ उपलब्ध'
+# print(pattern.search(str))  
+
+
+pattern1 = re.compile("[0-9]+|\w+(?= कक्षा)", re.IGNORECASE)
+str1 = "Class 10th Maths हिंदी Medium | UP Boards के लिए उपयोगी | सभी पाठ उपलब्ध हैं"
+a = pattern1.search(str1)
+print(a)
+
+# Thinkers, Beliefs and Buildings - One Shot Revision | Class 12 History Ch 4 in Hindi |UP/Bihar Board"
