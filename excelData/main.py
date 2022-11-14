a321ba700711a02317abd9b8477894752e06a9b1
aniketparate <aniketvparate@gmail.com>
2022-11-14 18:57:20 +0530

    refactored code by implementing oops

--- excelData/main.py
+++ excelData/main.py
@@ -1,91 +1,85 @@
 import pandas as pd
 import re
 
-# def use_regex(input_text):
-#     pattern = re.compile("हिंदी", re.IGNORECASE)
-#     a = pattern.search(input_text)
-
-#     if a:
-#         print(True)
-#     else:
-#         print(False)
-
-
-def findClass(id, input_text):
-    string = "(?<=class )[0-9]+", "(?<=कक्षा )[0-9]+"
-    i = 0
-    num = string.__len__()
-    while i != num:
-        pattern = re.compile(string[i], re.IGNORECASE)
-        srh = pattern.search(input_text)
-    
-        if srh == None:
-            pass
-            i+=1
-        else:
-            data.loc[id, ['class']] = [srh[0]]
-            # print(i)
-            # print(srh[0])
-            i+=1
-
-
-def findMedium(id, input_text):
-    string = "[\u0900-\u097F]+(?= medium)", "[A-Z][a-z]+(?= medium)", "(?<= medium)[A-Z][a-z]+", "(?<= medium )[\u0900-\u097F]+"
-    medium = "hindi", "english", "हिंदी", "अंग्रेज़ी"
-    num = string.__len__()
-    i = 0
-    while i != num:
-        pattern = re.compile(string[i], re.IGNORECASE)
-        srh = pattern.search(input_text)
+class findColumnData:
+    def findClass(self, id, input_text):
+        string = "(?<=class )[0-9]+", "(?<=कक्षा )[0-9]+"
+        i = 0
+        num = string.__len__()
+        while i != num:
+            pattern = re.compile(string[i], re.IGNORECASE)
+            srh = pattern.search(input_text)
         
-        for med in medium:
+            if srh == None:
+                pass
+                i+=1
+            else:
+                data.loc[id, ['class']] = [srh[0]]
+                i+=1
+
+    def findMedium(self, id, input_text):
+        string = "[\u0900-\u097F]+(?= medium)", "[A-Z][a-z]+(?= medium)", "(?<= medium)[A-Z][a-z]+", "(?<= medium )[\u0900-\u097F]+"
+        medium = "hindi", "english", "हिंदी", "अंग्रेज़ी"
+        num = string.__len__()
+        i = 0
+        while i != num:
+            pattern = re.compile(string[i], re.IGNORECASE)
+            srh = pattern.search(input_text)
+            
+            for med in medium:
+                if srh == None:
+                    i+=1
+                    pass
+                elif med == srh[0]:
+                    data.loc[id, ['medium']] = [srh[0]]
+                    i+=1
+                else:
+                    i+=1    
+        
+    def findSubject(self, id, input_text):
+        string = "(?<=class )[0-9]+ [A-Z][a-z]+", "(?<=class )[0-9]+[A-Z][a-z]+ [A-Z][a-z]+", "(?<=class )[0-9]+[A-Z][a-z]+ [\u0900-\u097F]+", "(?<=class )[0-9]+ [\u0900-\u097F]+"
+        subject ="Political"
+        i = 0
+        num = string.__len__()
+        while i != num:
+            pattern = re.compile(string[i], re.IGNORECASE)
+            srh = pattern.search(input_text)
+
             if srh == None:
                 i+=1
                 pass
-            elif med == srh[0]:
-                print(True, med, srh[0])
+            elif srh[0].split()[1].lower() == subject.lower():
                 i+=1
+                str = srh[0].split()[1] + ' Science'
+                data.loc[id, ['subject']] = [str]
             else:
-                print(False, med, srh[0])
-                i+=1    
-    
-def findSubject(id, input_text):
-    string = "(?<=class )[0-9]+ [A-Z][a-z]+", "(?<=class )[0-9]+[A-Z][a-z]+ [A-Z][a-z]+", "(?<=class )[0-9]+[A-Z][a-z]+ [\u0900-\u097F]+", "(?<=class )[0-9]+ [\u0900-\u097F]+"
-    subject ="Political"
-    i = 0
-    num = string.__len__()
-    while i != num:
-        pattern = re.compile(string[i], re.IGNORECASE)
+                i+=1
+                str = srh[0].split()[1]
+                data.loc[id, ['subject']] = [str]
+
+    def findAuthor(self, id, input_text):
+        pattern = re.compile("(?<=by )[A-Z][a-z]+ [A-Z][a-z]+", re.IGNORECASE)
         srh = pattern.search(input_text)
 
         if srh == None:
-            i+=1
             pass
-        elif srh[0].split()[1].lower() == subject.lower():
-            i+=1
-            print(srh[0].split()[1] + ' Science')
         else:
-            i+=1
-            print(srh[0].split()[1])
+            data.loc[id, ['author_name']] = [srh[0]]
 
-def findAuthor(id, input_text):
-    pattern = re.compile("(?<=by )[A-Z][a-z]+ [A-Z][a-z]+", re.IGNORECASE)
-    srh = pattern.search(input_text)
 
-    if srh == None:
-        pass
-    else:
-        print(srh[0])
+if __name__ == '__main__':
+    file = "magnet_brain_hindi.xlsx"
+    newUpdatedFile = "update_" + file
 
-file = "magnet_brain_hindi.xlsx"
-newUpdatedFile = "update_" + file
-data = pd.read_excel(file)
+    data = pd.read_excel(file)
 
-for i in data.itertuples():
-    # findClass(i[0], i[7])
-    # findMedium(i[0], i[7])
-    # findSubject(i[0], i[7])
-    # findAuthor(i[0], i[9])
+    colData = findColumnData()
 
-# data.to_excel(newUpdatedFile, index=False)
-# print(data)
+    for i in data.itertuples():
+        colData.findClass(i[0], i[7])
+        colData.findMedium(i[0], i[7])
+        colData.findSubject(i[0], i[7])
+        colData.findAuthor(i[0], i[16])
+
+    data.to_excel(newUpdatedFile, index=False)
+    print(data)
