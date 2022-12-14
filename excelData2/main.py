a4695b34dc553ce164a36e97fbc0c1fcfa8034dd
DishantWakalkar <dishantwakalkar@gmail.com>
2022-12-14 20:05:07 +0530

    Co-authored-by: Aniket Parate <aniketparate@users.noreply.github.com> Co-authored-by: HameedMulani <HameedMulani@users.noreply.github.com>

--- excelData2/main.py
+++ excelData2/main.py
@@ -1,8 +1,6 @@
 import pandas as pd
 import re
-# are mi te fix kela aata
-# are aaik toh bollela ki try kara ki maximum data aanaycha jar nasel yet tar rahude apan bolu tela 
-# aata konta lavkar bol
+
 class findColumnData: 
     def findClass(self, id, input_text):
         string = "(?<=class )[0-9]+", "(?<=CBSE )[0-9]+[a-z]+", "(?<=class )[0-9]+[a-z]+", "(?<=CBSE )[0-9]+", "[0-9]+[a-z]+(?= class)", "[0-9]+[a-z]+(?= CBSE)" 
@@ -38,45 +36,42 @@ class findColumnData:
     #             else:
     #                 i+=1
         
-    def findSubject(self, id, input_text):
+    def findSubject(self, id, input_text1 , input_text2):
         subject = "Economics", "Physics", "Maths", "Chemistry", "Biology", "Hindi", "Psychology", "Arts", "English"
-        # string = "(?<=class )[0-9]+ [A-Z][a-z]+", "(?<=class )[0-9]+[A-Z][a-z]+ [A-Z][a-z]+"
         i = 0
         num = subject.__len__()
         while i != num:
             string = "("+subject[i]+")"
             
-            pattern = re.compile(string[i], re.IGNORECASE)
-            srh = pattern.search(input_text)
+            pattern = re.compile(string, re.IGNORECASE)
+            srh1 = pattern.search(input_text1)
+            srh2 = pattern.search(input_text2)
 
-            if srh == None:
-                i+=1
-                pass
-            elif srh[0].split()[1].lower() == subject.lower():
-                i+=1
-                str = srh[0].split()[1] + ' Science'
-                data.loc[id, ['subject']] = [str]
+            if srh1 == None:
+                if srh2 == None:
+                    i+=1
+                    pass
+                else:
+                    i+=1
+                    data.loc[id, ['subject']] = [srh2[0]]
             else:
                 i+=1
-                str = srh[0].split()[1]
-                data.loc[id, ['subject']] = [str]
+                data.loc[id, ['subject']] = [srh1[0]]
+
 
     def findAuthor(self, id, input_text):
-        pattern = re.compile("(?<=by )[A-Z][a-z]+ [A-Z][a-z]+", re.IGNORECASE)
+        pattern = re.compile("(?<=by )[A-Z]{1}[a-z]+ [A-Z][a-z]+", re.IGNORECASE)
+        pattern2 = re.compile("[A-Z][a-z]+")
         srh = pattern.search(input_text)
 
         if srh == None:
             pass
         else:
-            data.loc[id, ['author_name']] = [srh[0]]
-
-    def findChapter(self, id, input_text):
-        pattern = re.compile("(?<=Chapter:\s)(.*?)\(", re.IGNORECASE)
-        srh = pattern.search(input_text)
-        if srh == None:
-            pass
-        else:
-            data.loc[id, ['chapter_name']] = [srh[0].split(r'/')[0].split('(')[0].split('-')[0]]
+            str = pattern2.search(srh[0])
+            if str == None:
+                pass
+            elif srh[0].split()[0] == str[0]:
+                data.loc[id, ['author_name']] = [srh[0]]
 
 if __name__ == '__main__':
     file = "usp_studio.xlsx"
@@ -89,9 +84,7 @@ if __name__ == '__main__':
     for i in data.itertuples():
         colData.findClass(i[0], i[8])
         # colData.findMedium(i[0], i[8])
-        colData.findSubject(i[0], i[8])
-        # colData.findChapter(i[0], i[15])
-        # colData.findAuthor(i[0], i[15])
+        colData.findSubject(i[0], i[7] ,i[8])
+        colData.findAuthor(i[0], i[15])
 
     data.to_excel(newUpdatedFile, index=False)
-    print(data)
